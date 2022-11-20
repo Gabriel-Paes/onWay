af626e2ca8137f80b6ad798ce0fc2842c6158a6e
Gabriel-Paes <paessouzagabriel@gmail.com>
2022-11-20 17:35:56 -0300

    Add home, css e routing

--- back/index.js
+++ back/index.js
@@ -1,1 +1,22 @@
-console.log("Test")
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+}
+
+const routes = {
+    "/front/index.html": "./home.html"
+};
+
+const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path];
+    const html = await fetch(route).then((data) => data.text());
+    document.querySelector('#main').innerHTML = html; 
+}
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
